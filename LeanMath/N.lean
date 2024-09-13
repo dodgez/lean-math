3438ce37d9957fcc4f251562be627c57cbec236d
Zachary Dodge <zdodge@amazon.com>
2024-09-12 22:24:18 -0700

    feat: AddCommMonoid for Z

--- LeanMath/N.lean
+++ LeanMath/N.lean
@@ -1,20 +1,25 @@
-import LeanMath.Algebra
+import Mathlib.Algebra.Group.Basic
 
 inductive N where
   | zero : N
   | succ : N -> N
 
-namespace N
-def add : N -> N -> N
+private def add : N -> N -> N
   | N.zero, n => n
   | N.succ m, n => N.succ $ add m n
+
 instance : Add N where
   add := add
 
-theorem zero_add (a : N) : N.zero + a = a := by
+private def mul : N -> N -> N
+  | N.zero, _ => N.zero
+  | N.succ n, m => m + mul n m
+
+namespace N
+@[simp] theorem zero_add (a : N) : N.zero + a = a := by
   simp only [HAdd.hAdd, Add.add, add]
 
-theorem add_succ (a b : N) : a + b.succ = (a + b).succ := by
+@[simp] theorem add_succ (a b : N) : a + b.succ = (a + b).succ := by
   simp only [HAdd.hAdd, Add.add]
   induction a
   case zero =>
@@ -52,14 +57,13 @@ theorem add_succ (a b : N) : a + b.succ = (a + b).succ := by
     simp only [HAdd.hAdd, Add.add, add]
     simp only [HAdd.hAdd, Add.add, add] at ih
     rw [ih]
-    have : ∀ (a b : N), (a.add b).succ = a.add b.succ := by
+    have : ∀ (a b : N), (a + b).succ = a + b.succ := by
       intro a b
       induction a
       case zero =>
-        simp only [add]
-      case succ _ ih =>
-        simp only [add]
-        rw [ih]
+        simp only [zero_add, add_succ]
+      case succ =>
+        simp only [succ_add, add_succ]
     apply this b n
 
 def nsmul : Nat -> N -> N
@@ -72,7 +76,7 @@ def nsmul : Nat -> N -> N
 @[simp] theorem nsmul_zero (a : N) : nsmul 0 a = zero := by
   simp only [nsmul]
 
-instance : Algebra.AddCommMonoid N where
+instance : AddCommMonoid N where
   add_assoc := add_assoc
   add_comm := add_comm
   add_zero := add_zero
@@ -82,10 +86,128 @@ instance : Algebra.AddCommMonoid N where
   zero := N.zero
   zero_add := zero_add
 
-instance : Algebra.AddCommSemiGroup N := Algebra.AddCommMonoid.toAddCommSemiGroup
-instance : Algebra.AddMonoid N := Algebra.AddCommMonoid.toAddMonoid
-instance : Algebra.AddSemiGroup N := Algebra.AddCommSemiGroup.toAddSemiGroup
-instance : Algebra.AddCommMagma N := Algebra.AddCommSemiGroup.toAddCommMagma
-instance : Algebra.AddZeroClass N := Algebra.AddMonoid.toAddZeroClass
-instance : Algebra.Zero N := Algebra.AddZeroClass.toZero
+@[simp] axiom succ_eq {a b : N} : a.succ = b.succ -> a = b
+
+@[simp] theorem add_right_cancel_iff {a b c : N} : a + c = b + c ↔ a = b := by
+  apply Iff.intro
+  case mp =>
+    intro h
+    induction c
+    case zero =>
+      simp only [N.add_zero] at h
+      simp only [h]
+    case succ n ih =>
+      simp only [N.add_succ] at h
+      rw [ih]
+      apply N.succ_eq
+      exact h
+  case mpr =>
+    intro h
+    induction c
+    case zero =>
+      simp only [h]
+    case succ _ ih =>
+      simp only [N.add_succ, ih]
+
+instance : Mul N where
+  mul := mul
+
+@[simp] theorem mul_comm (a b : N) : a * b = b * a := by
+  have mul_succ (a b : N) : mul a b.succ = a + mul a b := by
+    induction a
+    case zero =>
+      rfl
+    case succ a ih =>
+      simp only [mul]
+      rw [ih]
+      rw [<- N.succ_add]
+      repeat rw [<- N.succ_add]
+      rw [<- N.add_assoc]
+      rw [<- N.add_comm a b]
+      rw [<- N.add_assoc]
+  induction a
+  case zero =>
+    induction b
+    case zero =>
+      simp only [HMul.hMul, Mul.mul, mul]
+    case succ b ih =>
+      simp only [HMul.hMul, Mul.mul, mul] at *
+      rw [<- ih]
+      rfl
+  case succ a ih =>
+    induction b
+    case zero =>
+      simp only [HMul.hMul, Mul.mul, mul] at *
+      rw [ih]
+      rfl
+    case succ b _ =>
+      simp only [HMul.hMul, Mul.mul, mul] at *
+      rw [ih]
+      rw [mul_succ]
+      repeat rw [<- N.succ_add]
+      rw [<- N.add_assoc]
+      rw [<- N.add_comm a b]
+      rw [<- N.add_assoc]
+
+@[simp] theorem zero_mul (a : N) : zero * a = zero := by
+  simp only [HMul.hMul, Mul.mul, mul]
+
+@[simp] theorem mul_zero (a : N) : a * zero = zero := by
+  rw [<- N.mul_comm]
+  apply zero_mul
+
+@[simp] theorem mul_succ (a b : N) : a * succ b = a * b + a := by
+  induction a
+  case zero =>
+    simp only [zero_mul, add_zero]
+  case succ n ih =>
+    simp only [HMul.hMul, Mul.mul, mul] at *
+    rw [ih]
+    simp only [add_succ, succ_add, add_assoc]
+
+@[simp] theorem succ_mul (a b : N) : succ a * b = a * b + b := by
+  simp only [mul_comm, mul_succ]
+
+@[simp] theorem right_distrib (a b c : N) : (a + b) * c = a * c + b * c := by
+  induction c
+  case zero =>
+    simp only [mul_zero, add_zero]
+  case succ n ih =>
+    repeat rw [mul_succ]
+    rw [ih]
+    rw [add_assoc]
+    rw [<- add_assoc (b*n) a b]
+    rw [add_comm (b*n)]
+    repeat rw [<- add_assoc]
+
+@[simp] theorem mul_assoc (a b c : N) : a * b * c = a * (b * c) := by
+  induction a
+  case zero =>
+    simp only [HMul.hMul, Mul.mul, mul]
+  case succ a ih =>
+    induction b
+    case zero =>
+      simp only [mul_zero, zero_mul]
+    case succ b _ =>
+      rw [succ_mul]
+      rw [right_distrib]
+      rw [ih]
+      simp only [succ_mul]
+
+private def one := N.succ 0
+
+@[simp] theorem one_mul (a : N) : one * a = a := by
+  simp only [HMul.hMul, Mul.mul, mul, N.add_zero]
+
+@[simp] theorem mul_one (a : N) : a * one = a := by
+  rw [<- mul_comm]
+  apply one_mul
+
+instance : CommMonoid N where
+  mul := mul
+  mul_assoc := mul_assoc
+  mul_comm := mul_comm
+  mul_one := mul_one
+  one := one
+  one_mul := one_mul
 end N
--- LeanMath/Z.lean
+++ LeanMath/Z.lean
@@ -1,31 +1,247 @@
+import Mathlib.Algebra.Group.Basic
+import Mathlib.Algebra.Ring.Basic
 import LeanMath.N
 
-def r : (N × N) -> (N × N) -> Prop
-  | (n1, m1), (n2, m2) => n1 + m2 = n2 + m1
+private def N₂ := N × N
 
-@[simp] axiom N.add_right_cancel : ∀ (a b c : N), a + c = b + c ↔ a = b
-#check Nat.add_right_cancel
-#check Nat.add_right_cancel_iff
+private def eqv (n m : N₂) : Prop :=
+  n.1 + m.2 = n.2 + m.1
 
-instance : Equivalence r where
-  refl := by
-    simp only [r, implies_true]
-  symm := by
-    intro x y
-    simp only [r]
-    intro h
-    rw [h]
-  trans := by
-    intro x y z
-    intro h1 h2
-    simp [r]
-    simp [r] at h1 h2
+@[simp] private theorem eqv_is_refl (x : N₂) :  eqv x x := by
+  simp only [eqv, N.add_comm]
 
-instance : Setoid (N × N) where
-  iseqv := instEquivalenceProdNR
-  r := r
+@[simp] private theorem eqv_is_symm {x y : N₂} : eqv x y -> eqv y x := by
+  intro h
+  simp only [eqv, N.add_comm] at *
+  simp only [h]
 
+@[simp] private theorem eqv_is_trans {x y z : N₂} : eqv x y -> eqv y z -> eqv x z := by
+  intro h1
+  intro h2
+  simp only [eqv] at *
+  apply Iff.mp $ N.add_right_cancel_iff (c := y.snd)
+  have : x.fst + y.snd + z.snd = x.snd + y.fst + z.snd :=
+    Iff.mpr N.add_right_cancel_iff h1
+  simp only [N.add_assoc, N.add_comm z.snd]
+  simp only [<- N.add_assoc]
+  simp only [this]
+  simp only [N.add_assoc]
+  simp only [h2]
+  simp only [N.add_comm]
 
-def Z := Quotient instSetoidProdN
-def Z.mk : (N × N) -> Z := Quotient.mk instSetoidProdN
-#check Z.mk (N.zero, N.zero)
+@[simp] private theorem eqv_iseqv : Equivalence eqv := {
+  refl := eqv_is_refl
+  symm := eqv_is_symm
+  trans := eqv_is_trans
+}
+
+private instance : Setoid N₂ where
+  iseqv := eqv_iseqv
+  r := eqv
+
+def Z := Quotient instSetoidN₂
+def Z.mk : (N × N) -> Z := Quotient.mk instSetoidN₂
+
+private def add : (N × N) -> (N × N) -> Z
+  | (a, b), (c, d) => Z.mk (a + c, b + d)
+private theorem add_is_well_defined (a₁ b₁ a₂ b₂ : N × N) (h1 : eqv a₁ a₂) (h2 : eqv b₁ b₂) : add a₁ b₁ = add a₂ b₂ := by
+  simp only [eqv, add] at *
+  apply Quotient.sound
+  simp only [instHasEquivOfSetoid, Setoid.r, eqv]
+  rw [N.add_assoc]
+  rw [<- N.add_assoc b₁.fst]
+  rw [N.add_comm b₁.fst]
+  repeat rw [<- N.add_assoc]
+  rw [h1]
+  rw [N.add_assoc]
+  rw [h2]
+  rw [N.add_assoc]
+  rw [<- N.add_assoc a₂.fst]
+  rw [N.add_comm a₂.fst]
+  repeat rw [<- N.add_assoc]
+
+private def zAdd : Z -> Z -> Z := Quotient.lift₂ add add_is_well_defined
+
+private theorem add_is_lifted_addN₂ : ∀ (x y : N₂), zAdd (Z.mk x) (Z.mk y) = add x y := by
+  intro x y
+  let (x1, x2) := x
+  let (y1, y2) := y
+  apply Quotient.sound
+  apply Equivalence.refl eqv_iseqv
+
+private def neg : (N × N) -> Z
+  | (a, b) => Z.mk (b, a)
+
+private theorem neg_is_well_defined (a b : N × N) (h : eqv a b) : neg a = neg b := by
+  simp only [eqv, add] at *
+  apply Quotient.sound
+  simp only [instHasEquivOfSetoid, Setoid.r, eqv]
+  rw [h]
+
+private def zNeg : Z -> Z := Quotient.lift neg neg_is_well_defined
+
+private theorem neg_is_lifted_negN₂ : ∀ (x : N₂), zNeg (Z.mk x) = neg x := by
+  intro x
+  let (x1, x2) := x
+  apply Quotient.sound
+  apply Equivalence.refl eqv_iseqv
+
+namespace Z
+instance : Add Z where
+  add := zAdd
+
+def zero := Z.mk (N.zero, N.zero)
+
+@[simp] theorem zero_add (a : Z) : zero + a = a := by
+  have zero_works : (mk (N.zero, N.zero)) = zero := by
+    rfl
+  cases (Quotient.exists_rep a) with
+  | intro aPos h => {
+    rw [<- h]
+    rw [<- zero_works]
+    rw [<- mk]
+    simp only [HAdd.hAdd, Add.add]
+    rw [add_is_lifted_addN₂ (N.zero, N.zero) aPos]
+    unfold add
+    let (a1, a2) := aPos
+    simp
+  }
+
+@[simp] theorem add_zero (a : Z) : a + zero = a := by
+  have zero_works : (mk (N.zero, N.zero)) = zero := by
+    rfl
+  cases (Quotient.exists_rep a) with
+  | intro aPos h => {
+    rw [<- h]
+    rw [<- zero_works]
+    rw [<- mk]
+    simp only [HAdd.hAdd, Add.add]
+    rw [add_is_lifted_addN₂ aPos (N.zero, N.zero)]
+    unfold add
+    let (a1, a2) := aPos
+    simp
+  }
+
+@[simp] theorem add_comm (a b : Z) : a + b = b + a := by
+  cases (Quotient.exists_rep a) with
+  | intro aPos h1 => {
+    cases (Quotient.exists_rep b) with
+    | intro bPos h2 => {
+      rw [<- h1, <- h2]
+      apply Quotient.sound
+      simp only [N.add_comm]
+      apply Equivalence.refl eqv_iseqv
+    }
+  }
+
+def nsmul : Nat -> Z -> Z
+  | Nat.succ n, a => nsmul n a + a
+  | Nat.zero, _ => Z.zero
+
+@[simp] theorem nsmul_succ (n : Nat) (a : Z) : nsmul n.succ a = nsmul n a + a := by
+  simp only [nsmul]
+
+@[simp] theorem nsmul_zero (a : Z) : nsmul 0 a = Z.zero := by
+  simp only [nsmul]
+
+theorem add_assoc (a b c : Z) : a + b + c = a + (b + c) := by
+  cases (Quotient.exists_rep a) with
+  | intro aPos h1 => {
+    let (a1, a2) := aPos
+    cases (Quotient.exists_rep b) with
+    | intro bPos h2 => {
+      let (b1, b2) := bPos
+      cases (Quotient.exists_rep c) with
+      | intro cPos h3 => {
+        let (c1, c2) := cPos
+        rw [<- h1, <- h2, <- h3]
+        rw [<- Z.mk]
+        apply Quotient.sound
+        repeat rw [N.add_assoc]
+        apply Equivalence.refl eqv_iseqv
+      }
+    }
+  }
+
+instance : AddCommMonoid Z where
+  add_assoc := add_assoc
+  add_comm := add_comm
+  add_zero := add_zero
+  nsmul := nsmul
+  nsmul_succ := nsmul_succ
+  nsmul_zero := nsmul_zero
+  zero := Z.mk (N.zero, N.zero)
+  zero_add := zero_add
+
+instance : Neg Z where
+  neg := zNeg
+
+@[simp] theorem neg_add_cancel (a : Z) : -a + a = Z.zero := by
+  cases Quotient.exists_rep a with
+  | intro aPos h => {
+    let (a1, a2) := aPos
+    rw [<- h]
+    rw [<- Z.mk]
+    apply Quotient.sound
+    rw [N.add_comm]
+    rfl
+  }
+
+def zsmul : ℤ -> Z -> Z
+  | (Int.negSucc n), a => -nsmul (n + 1) a
+  | (Int.ofNat Nat.zero), _ => 0
+  | (Int.ofNat (Nat.succ n)), a => a + (nsmul n a)
+
+@[simp] private theorem zsmul_neg' (n : Nat) (a : Z) : zsmul (Int.negSucc n) a = -zsmul (↑n.succ) a := by
+  unfold zsmul
+  simp only [nsmul_succ, add_comm]
+
+@[simp] private theorem zsmul_succ' (n : Nat) (a : Z) : zsmul (Int.ofNat n.succ) a = zsmul (Int.ofNat n) a + a := by
+  unfold zsmul
+  simp only [Int.ofNat_eq_coe, nsmul_succ, add_comm]
+  induction n
+  case zero =>
+    simp only [nsmul_zero]
+    rfl
+  case succ n _ =>
+    simp only [nsmul_succ, add_comm]
+
+@[simp] private theorem zsmul_zero' (a : Z) : zsmul 0 a = 0 := by
+  rfl
+
+instance : AddGroup Z where
+  neg_add_cancel := neg_add_cancel
+  zsmul := zsmul
+  zsmul_neg' := zsmul_neg'
+  zsmul_succ' := zsmul_succ'
+  zsmul_zero' := zsmul_zero'
+
+instance : CommMonoid Z where
+  mul := sorry
+  mul_assoc := sorry
+  mul_comm := sorry
+  mul_one := sorry
+  one := sorry
+  one_mul := sorry
+
+instance : CommRing Z where
+  add_comm := add_comm
+  add_zero := add_zero
+  left_distrib := sorry
+  mul_assoc := sorry
+  mul_comm := sorry
+  mul_one := sorry
+  mul_zero := sorry
+  neg_add_cancel := neg_add_cancel
+  nsmul := nsmul
+  nsmul_succ := nsmul_succ
+  nsmul_zero := nsmul_zero
+  one_mul := sorry
+  right_distrib := sorry
+  zero_add := zero_add
+  zero_mul := sorry
+  zsmul := zsmul
+  zsmul_neg' := zsmul_neg'
+  zsmul_succ' := zsmul_succ'
+  zsmul_zero' := zsmul_zero'
+end Z
